6cc412a24382b684541e0181c1a4ecc021b26f95
edram <edram@qq.com>
2022-09-07 01:02:53 +0000

    feat: 1592. 重新排列单词间的空格(ts)

---
+++ leetcode/1592-rearrange-spaces-between-words/reorderSpaces.ts
@@ -0,0 +1,16 @@
+function reorderSpaces(text: string): string {
+  const tempText = text.split(" ");
+  const words = tempText.filter((item) => item !== "");
+  const wordsCount = words.length;
+  const spaceCount = text.length - words.join("").length;
+  if (wordsCount <= 1) {
+    return `${words.join("")}${" ".repeat(spaceCount)}`;
+  }
+
+  const betweenSpaceCount = Math.floor(spaceCount / (words.length - 1));
+  const restSpace = spaceCount % (words.length - 1);
+
+  return `${words.join(" ".repeat(betweenSpaceCount))}${" ".repeat(restSpace)}`;
+}
+
+export default reorderSpaces;
