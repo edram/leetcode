890958079fc3268a59f2971225808399a19f1e23
edram <edram@qq.com>
2022-09-09 15:06:21 +0000

    fix: 修复 test name

--- leetcode/1598-crawler-log-folder/minOperations.test.ts
+++ leetcode/1598-crawler-log-folder/minOperations.test.ts
@@ -1,7 +1,7 @@
 import cases from "./cases.json";
 import minOperations from "./minOperations";
 
-test("rearrange spaces between words", () => {
+test("crawler log folder", () => {
   cases.forEach((testCase, index) => {
     console.log(`正在执行第 ${index + 1} 个测试用例\n`);
     expect(minOperations(testCase.input)).toEqual(testCase.output);
