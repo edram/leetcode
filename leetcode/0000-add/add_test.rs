e42a0e89a1ecf21f291f502dd62a143e99221820
edram <edram@qq.com>
2024-03-18 18:06:06 +0800

    chore: 增加 rust 简单测试用例

---
+++ leetcode/0000-add/add_test.rs
@@ -0,0 +1,8 @@
+use crate::leetcode::add::add;
+
+#[test]
+fn test_bad_add2() {
+    // This assert would fire and test will fail.
+    // Please note, that private functions can be tested too!
+    assert_eq!(add(1,2), 3);
+}
---
+++ lib.rs
@@ -0,0 +1,13 @@
+pub mod leetcode {
+  #[path = "0000-add/add.rs"]
+  pub mod add;
+
+
+  #[cfg(test)]
+  #[path = ""]
+  mod rust {
+      #[path = "0000-add/add_test.rs"]
+      mod add_test;
+  }
+}
+
