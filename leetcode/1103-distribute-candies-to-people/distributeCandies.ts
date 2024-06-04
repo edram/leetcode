a14209aaa86651cb60d68290e39d3a8563bd1ddd
edram <edram@qq.com>
2024-06-04 14:29:12 +0800

    fix: 根据测试用例3调整代码 (算法有问题)

--- leetcode/1103-distribute-candies-to-people/distributeCandies.ts
+++ leetcode/1103-distribute-candies-to-people/distributeCandies.ts
@@ -1,13 +1,14 @@
 function distributeCandies(candies: number, num_people: number): number[] {
   const result = new Array<number>(num_people).fill(0);
 
-  const times = Math.ceil(Math.sqrt(2 * candies - 1)); // 分糖果的次数
+  const times = Math.floor(Math.sqrt(2 * candies - 1)); // 最多能完美分的次数
   const round = Math.ceil(times / num_people); // 每人最多轮到的次数
   const rest = round * num_people - times; // 剩下几人没有轮到最后一轮
-  const left = candies - ((1 + times - 1) * (times - 1)) / 2;
+  const left = candies - ((1 + times) * times) / 2;
+  const restCol = num_people - rest - 1;
 
   for (let index = 0; index < num_people; index++) {
-    const itemRound = index < num_people - rest - 1 ? round : round - 1;
+    const itemRound = index <= restCol ? round : round - 1;
     const last = index + 1 + (itemRound - 1) * num_people;
 
     const num = ((index + 1 + last) / 2) * itemRound;
@@ -16,7 +17,12 @@ function distributeCandies(candies: number, num_people: number): number[] {
   }
 
   // 其余都是等差数列求和，最后一人拿走剩下的糖果
-  result[num_people - rest - 1] = result[num_people - rest - 1] + left;
+  // 或者是最后一个多拿了，从最后一个人拿拿回来
+  if (left > 0) {
+    result[restCol + 1] = result[restCol + 1] + left;
+  } else {
+    result[restCol] = result[restCol] + left;
+  }
 
   return result;
 }
