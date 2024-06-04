2ec1c8563b6aed15da3800eb153145fa64a82340
edram <edram@qq.com>
2024-06-04 16:12:43 +0800

    fix: 计算 times 出错，Δ 求根公式

--- leetcode/1103-distribute-candies-to-people/distributeCandies.ts
+++ leetcode/1103-distribute-candies-to-people/distributeCandies.ts
@@ -1,28 +1,28 @@
 function distributeCandies(candies: number, num_people: number): number[] {
   const result = new Array<number>(num_people).fill(0);
 
-  const times = Math.floor(Math.sqrt(2 * candies - 1)); // 最多能完美分的次数
-  const round = Math.ceil(times / num_people); // 每人最多轮到的次数
-  const rest = round * num_people - times; // 剩下几人没有轮到最后一轮
-  const left = candies - ((1 + times) * times) / 2;
-  const restCol = num_people - rest - 1;
+  const times = Math.floor(Math.sqrt(2 * candies + 0.25) - 0.5); // 能完美分的次数
+  const round = Math.floor(times / num_people); // 所有人都轮到完美分的次数
+  const restCol = times % num_people; // 剩下可以完美论分的次数
 
+  const left = candies - Math.floor((times + 1) * times * 0.5);
+
+  // 处理每人都轮到的
   for (let index = 0; index < num_people; index++) {
-    const itemRound = index <= restCol ? round : round - 1;
-    const last = index + 1 + (itemRound - 1) * num_people;
+    const last = index + 1 + (round - 1) * num_people;
 
-    const num = ((index + 1 + last) / 2) * itemRound;
+    const num = ((index + 1 + last) / 2) * round;
 
     result[index] = num;
   }
 
-  // 其余都是等差数列求和，最后一人拿走剩下的糖果
-  // 或者是最后一个多拿了，从最后一个人拿拿回来
-  if (left > 0) {
-    result[restCol + 1] = result[restCol + 1] + left;
-  } else {
-    result[restCol] = result[restCol] + left;
+  // 处理最后一轮
+  for (let index = 0; index < restCol; index++) {
+    result[index] += index + 1 + round * num_people;
   }
+
+  // 处理剩下的
+  result[restCol] += left;
 
   return result;
 }
