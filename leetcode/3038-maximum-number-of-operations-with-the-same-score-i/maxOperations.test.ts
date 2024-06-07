a88555066de95eabc76b5ccd82d69bca2041d1ca
edram <edram@qq.com>
2024-06-07 08:38:43 +0800

    wip: 3038. 相同分数的最大操作数目 I

---
+++ leetcode/3038-maximum-number-of-operations-with-the-same-score-i/maxOperations.test.ts
@@ -0,0 +1,16 @@
+import cases from "./cases.json";
+import maxOperations from "./maxOperations";
+
+test("maximum number of operations with the same score i", () => {
+  cases.forEach((testCase, index) => {
+    console.log(`正在执行第 ${index + 1} 个测试用例\n`);
+    console.time("time");
+    const result = maxOperations(testCase.input);
+    console.timeEnd("time");
+    console.log(
+      `input: ${testCase.input} , output: ${testCase.output} ==> ${result} \n`
+    );
+
+    expect(result).toEqual(testCase.output);
+  });
+});
---
+++ leetcode/3038-maximum-number-of-operations-with-the-same-score-i/maxOperations.ts
@@ -0,0 +1,15 @@
+function maxOperations(nums: number[]): number {
+  let times = 1;
+  const sum = nums[0] + nums[1];
+
+  for (let index = 2; index < nums.length - 1; index = index + 2) {
+    if (nums[index] + nums[index + 1] != sum) {
+      continue;
+    }
+    times++;
+  }
+
+  return times;
+}
+
+export default maxOperations;
